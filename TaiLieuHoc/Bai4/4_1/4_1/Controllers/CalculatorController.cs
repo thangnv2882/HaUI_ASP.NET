d1221d1463eea97f1dfbbb127498e0a2dd6a7c37
Thang Nguyen Van <thang.nv2882@gmail.com>
2022-10-15 10:47:00 +0700

    add buoi 5+6

---
+++ TaiLieuHoc/Bai4/4_1/4_1/Controllers/CalculatorController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebGrease.Activities;
+
+namespace _4_1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: Caculator
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Calculate()
+        {
+            double a = double.Parse(Request["soA"]);
+            double b = double.Parse(Request["soB"]);
+
+            System.Diagnostics.Debug.WriteLine(a);
+
+            ViewBag.kq = a;
+
+            return View("Index");
+        }
+    }
+}
