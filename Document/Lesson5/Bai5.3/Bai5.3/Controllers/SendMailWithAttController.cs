f8dc00a7dca0397c3dc3854eefc1204235b96ef2
Thang Nguyen Van <thang.nv2882@gmail.com>
2022-10-27 07:10:47 +0700

    add btvn b6

---
+++ Document/Lesson5/Bai5.3/Bai5.3/Controllers/SendMailWithAttController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Bai5._3.Models;
+using System.Net.Mime;
+using System.Web.Services.Description;
+using System.IO;
+using System.Web.UI.WebControls;
+
+namespace Bai5._3.Controllers
+{
+    public class SendMailWithAttController : Controller
+    {
+        // GET: SendMailWithAtt
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(MailInfo model)
+        {
+            using (MailMessage mail = new MailMessage())
+            {
+                mail.From = new MailAddress(model.From);
+                mail.To.Add(model.To);
+                mail.Subject = model.Subject;
+                mail.Body = model.Note;
+                mail.IsBodyHtml = true;
+
+                //Lấy thông tin từ input type = size
+                var fAtt = Request.Files["myAttachment"];
+
+                // Save hinh về server
+                var pathAtt = Server.MapPath("~/Attachment/" + fAtt.FileName);
+                fAtt.SaveAs(pathAtt);
+
+                Attachment data = new Attachment(
+                         "~/Attachment/" + fAtt.FileName,
+                         MediaTypeNames.Application.Octet);
+                mail.Attachments.Add(data);
+
+                // Can set to false, if you are sending pure text.
+                using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    // Email: tgtg2808@gmail.com
+                    // Password: amoqkqczfjexcizd
+                    smtp.Credentials = new NetworkCredential(model.From, "amoqkqczfjexcizd");
+                    System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
+                }
+            }
+
+            return RedirectToAction("Index", "SendMailWithAtt");
+
+        }
+    }
+}
